ddf53012e1b98284263d1ed8fea9430811185fa7
nschumac <schumachernikolai@gmail.com>
2024-01-08 09:30:38 +0100

    initial commit

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,82 @@
+import java.util.HashSet;
+
 public class ArrayOps {
     public static void main(String[] args) {
         
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
+        // + 1 becuase array is length of n but 
+        // i also need to store 0
+        boolean[] characterFoundArray = new boolean[array.length + 1];
+
+        for (int i = 0; i < array.length; ++i) {
+            characterFoundArray[array[i]] = true;
+        }
+
+        for (int i = 0; i < characterFoundArray.length; ++i) {
+            // number is missing in array
+            if (!characterFoundArray[i]) {
+                return i;
+            }
+        }
         return -1;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int largest, secondLargest;
+
+        largest = secondLargest = array[0];
+        for (int i = 1; i < array.length; ++i) {
+            if (array[i] > secondLargest) {
+                secondLargest = array[i];
+            }
+            if (array[i] > largest) {
+                secondLargest = largest;
+                largest = array[i];
+            }
+        }
+        return secondLargest;
+    }
+
+    private static boolean arrayContainsInt(int [] array, int integerToFind) {
+        for (int i = 0; i < array.length; ++i) {
+            if (array[i] == integerToFind) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        for (int i = 0; i < array1.length; ++i) {
+            if (!arrayContainsInt(array2, array1[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+
+        if (array.length <= 1) {
+            return true;
+        }
+    
+        boolean increaseOrDecrease = array[0] < array[1];
+
+        for (int i = 1; i < array.length; ++i) {
+            if (increaseOrDecrease) {
+                if (array[i - 1] > array[i]) {
+                    return false;
+                }
+            } else {
+                if (array[i - 1] < array[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -25,18 +25,96 @@ public class StringOps {
         
     }
 
+    private static char characterToLowerCase(char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return (char)(c + 32);
+        }
+        return c;
+    }
+
+    private static char characterToUpperCase(char c) {
+        if (c >= 'a' && c <= 'z') {
+            return (char)(c - 32);
+        }
+        return c;
+    }
+
+    private static boolean characterIsVowel(char c) {
+        switch (c) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                return true;
+            default:
+                return false;
+        }
+    }
+
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String result = "";
+
+        for (int i = 0; i < string.length(); ++i) {
+            if (characterIsVowel(string.charAt(i))) {
+                result += characterToUpperCase(string.charAt(i));
+            } else {
+                result += characterToLowerCase(string.charAt(i));
+            }
+        }
+        return result;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String result = "";
+
+        for (int i = 0; i < string.length(); ++i) {
+            // trim spaces
+            while (string.charAt(i) == ' ') {
+                ++i;
+            }
+            // we found first word already
+            if (result != "") {
+                result += characterToUpperCase(string.charAt(i));
+            } else {
+                result += characterToLowerCase(string.charAt(i));
+            }
+        }
+        return result;
     }
 
+    // Ask if allowed to use ArrayList<int> would be much better
+    // wouldn't need two loops
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int indexOfCharacter = -1;
+        int indexesFound = 0;
+        
+        // count number of characters that we have in the string
+        do {
+            indexOfCharacter = string.indexOf(chr, indexOfCharacter + 1);
+            if (indexOfCharacter != -1) {
+                indexesFound++;
+            }
+        } while (indexOfCharacter != -1); // didn't find character
+
+
+        // result is amount of indexes found :shrug:
+        int[] result = new int[indexesFound];
+        int indexIndex = 0;
+
+        // set the indices in the result array
+        do {
+            indexOfCharacter = string.indexOf(chr, indexOfCharacter + 1);
+            if (indexOfCharacter != -1) {
+                result[indexIndex++] = indexOfCharacter;
+            }
+        } while (indexOfCharacter != -1); // didn't find character
+
+        return result;
     }
 }
